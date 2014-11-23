a41178378bebc9014f1de34dbaf5876af57fbf63
Madan <dmadan86@gmail.com>
2014-11-23 19:47:13 +0530

    - Implementation of bubble sort in javascript and HTML

---
+++ bubble sort/javascript/bubble.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+
+	var arr = [],
+		len = 25000,
+		i=0,
+		temp,
+		j=0,
+		start,
+		stop;
+
+	//generating random number
+	for(i=0;i<len;i++){
+		arr.push((Math.random() % len)+1);	
+	}
+
+	start = new Date();
+	//bubble sort
+	for(i=1;i<len;i++){
+		for(j=0;j<len-1;j++){
+			if(arr[j] > arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}		
+		}
+	}
+	stop = new Date();
+
+	document.getElementById("output").innerHTML = ("Sorted array length: " + len + ", time taken to sort(bubble sort): " + (stop - start) + " ms");
+};
